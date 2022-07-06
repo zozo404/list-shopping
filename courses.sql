732b2a07f244b4858d50454669ed0d22932c0c2a
zozo404 <enzocuny66@gmail.com>
2022-07-06 16:23:48 +0200

    :globe_with_meridians: | update bdd

--- courses.sql
+++ courses.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 29 juin 2022 à 09:33
+-- Généré le : mer. 06 juil. 2022 à 14:22
 -- Version du serveur : 5.7.33
--- Version de PHP : 7.4.19
+-- Version de PHP : 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,7 +40,9 @@ INSERT INTO `magasin` (`id_magasin`, `nom_magasin`) VALUES
 (1, 'Leclerc'),
 (2, 'Carrefour'),
 (3, 'Auchan'),
-(4, 'Lidl');
+(4, 'Lidl'),
+(5, 'Intermarché'),
+(6, 'Super U');
 
 -- --------------------------------------------------------
 
@@ -51,23 +53,21 @@ INSERT INTO `magasin` (`id_magasin`, `nom_magasin`) VALUES
 CREATE TABLE `produit` (
   `id_produit` int(11) UNSIGNED NOT NULL,
   `nom_produit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `quantite_produit` int(11) UNSIGNED NOT NULL
+  `quantite_produit` int(11) UNSIGNED NOT NULL,
+  `fk_id_magasin` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id_produit`, `nom_produit`, `quantite_produit`) VALUES
-(1, 'Pain', 2),
-(2, 'Orange', 5),
-(3, 'Jus de fruits', 1),
-(4, 'Paquet de chips', 3),
-(5, 'Gateaux', 1),
-(6, 'Fromage', 1),
-(7, 'Pommes de terre', 5),
-(9, 'fuze tea pêche', 4),
-(10, 'cocaïne', 4);
+INSERT INTO `produit` (`id_produit`, `nom_produit`, `quantite_produit`, `fk_id_magasin`) VALUES
+(2, 'Orange', 6, 0),
+(3, 'Jus de fruits', 1, 2),
+(6, 'Fromage', 1, 3),
+(7, 'Pommes de feu', 8, 1),
+(14, 'Fuze tea', 45, 1),
+(24, 'eau', 2, 4);
 
 --
 -- Index pour les tables déchargées
@@ -83,7 +83,8 @@ ALTER TABLE `magasin`
 -- Index pour la table `produit`
 --
 ALTER TABLE `produit`
-  ADD PRIMARY KEY (`id_produit`);
+  ADD PRIMARY KEY (`id_produit`),
+  ADD KEY `fk_id_magasin` (`fk_id_magasin`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -93,13 +94,13 @@ ALTER TABLE `produit`
 -- AUTO_INCREMENT pour la table `magasin`
 --
 ALTER TABLE `magasin`
-  MODIFY `id_magasin` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_magasin` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `produit`
 --
 ALTER TABLE `produit`
-  MODIFY `id_produit` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_produit` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
